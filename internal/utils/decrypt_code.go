fca508f57e08f979db2b1a405793b5bd3780ad44
Johann six <six.johann@gmail.com>
2024-08-28 14:59:12 +0200

    add decrypt code func and test

---
+++ internal/utils/decrypt_code.go
@@ -0,0 +1,24 @@
+package utils
+
+import (
+	"regexp"
+)
+
+func decryptCode(secret string, secretLength int, code string) bool {
+	regex, err := regexp.Compile(secret)
+	if err != nil {
+		panic(err)
+	}
+
+	// Check if the code respects the secret regex
+	if !regex.MatchString(code) {
+		return false
+	}
+
+	// Check if the code has the correct length
+	if len(code) != secretLength {
+		return false
+	}
+
+	return true
+}
---
+++ internal/utils/decrypt_code_test.go
@@ -0,0 +1,73 @@
+package utils
+
+import "testing"
+
+func TestDecryptCode(t *testing.T) {
+
+	tests := []struct {
+		name         string
+		secret       string
+		secretLength int
+		code         string
+		expected     bool
+	}{
+		{
+			name:         "valid code",
+			secret:       "^[0-9]{6}$",
+			secretLength: 6,
+			code:         "123456",
+			expected:     true,
+		},
+		{
+			name:         "invalid code (does not match regex)",
+			secret:       "^[0-9]{6}$",
+			secretLength: 6,
+			code:         "abcdef",
+			expected:     false,
+		},
+		{
+			name:         "invalid code (wrong length)",
+			secret:       "^[0-9]$",
+			secretLength: 6,
+			code:         "12345",
+			expected:     false,
+		},
+		{
+			name:         "invalid code (empty)",
+			secret:       "^[0-9]$",
+			secretLength: 6,
+			code:         "",
+			expected:     false,
+		},
+		{
+			name:         "valid code 2 letters and 6 numbers",
+			secret:       "^[a-zA-Z]{2}[0-9]{6}$",
+			secretLength: 8,
+			code:         "ab123456",
+			expected:     true,
+		},
+		{
+			name:         "valid code 6 numbers and 2 letters",
+			secret:       "^[0-9]{6}[a-zA-Z]{2}$",
+			secretLength: 8,
+			code:         "123456ab",
+			expected:     true,
+		},
+		{
+			name:         "invalid code 6 numbers and 2 letters",
+			secret:       "^[0-9]{6}[a-zA-Z]{2}$",
+			secretLength: 8,
+			code:         "ab123456",
+			expected:     false,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			result := decryptCode(tt.secret, tt.secretLength, tt.code)
+			if result != tt.expected {
+				t.Errorf("Expected %t, but got %t", tt.expected, result)
+			}
+		})
+	}
+}

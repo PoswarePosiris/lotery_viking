8013efdbe49b3ecf1add4aebb6f209b876673a13
Johann six <six.johann@gmail.com>
2024-08-28 14:59:20 +0200

    move models folder

--- models/publicity_images.go
+++
@@ -1,7 +0,0 @@
-package models
-
-type PublicityImages struct {
-	BaseModel
-	ParameterId *Parameters `db:"parameter_id" json:"parameter"`
-	ImageId     *Images     `db:"image_id" json:"image"`
-}
--- models/rewards.go
+++
@@ -1,7 +0,0 @@
-package models
-
-type Rewards struct {
-	BaseModel
-	Name     string  `db:"name" json:"name"`
-	IdImages *Images `db:"id_images" json:"image,omitempty"`
-}

81fc393e9aca42d513d646a4328a941a895ca307
Johann six <six.johann@gmail.com>
2024-08-28 14:59:51 +0200

    update func with refacto

--- internal/testing.go
+++ internal/testing.go
@@ -1,9 +1,10 @@
 package internal
 
 import (
-	"fmt"
+	"bytes"
 	"io"
-	"net/http"
+	"net/http/httptest"
+	"strings"
 	"testing"
 )
 
@@ -14,9 +15,8 @@ func AssertStatusCode(t *testing.T, got, want int) {
 	}
 }
 
-func AssertResponseBody(t *testing.T, body io.ReadCloser, expected string) {
+func AssertResponseBody(t *testing.T, body *bytes.Buffer, expected string) {
 	t.Helper()
-	defer body.Close()
 
 	b, err := io.ReadAll(body)
 	if err != nil {
@@ -28,15 +28,17 @@ func AssertResponseBody(t *testing.T, body io.ReadCloser, expected string) {
 }
 
 // assertContentType checks if the response content-type matches the expected value
-func AssertContentType(t testing.TB, response *http.Response, want string) {
+func AssertContentType(t testing.TB, response *httptest.ResponseRecorder, want string) {
 	t.Helper()
-	if response.Header.Get("content-type") != want {
-		t.Errorf("response did not have content-type of %s, got %v", want, response.Header)
+	contentType := response.Header().Get("content-type")
+	if !strings.HasPrefix(contentType, want) {
+		t.Errorf("response did not have content-type of %s, got %s", want, contentType)
 	}
 }
 
-// newRequestPath creates a new HTTP GET request for the given path
-func NewRequestPath(url string) *http.Request {
-	req, _ := http.NewRequest(http.MethodGet, fmt.Sprintf("/%s", url), nil)
-	return req
+func AssertContextValue(t *testing.T, got, want interface{}) {
+	t.Helper()
+	if got != want {
+		t.Errorf("expected context value %v; got %v", want, got)
+	}
 }

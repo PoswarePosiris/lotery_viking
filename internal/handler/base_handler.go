c611a6a2e4ec9ed8068f4e606306c431fa476237
Johann six <six.johann@gmail.com>
2024-08-29 16:17:50 +0200

    add new field in th request

--- internal/handler/base_handler.go
+++ internal/handler/base_handler.go
@@ -23,10 +23,10 @@ func (b *BaseHandler) getKioskId(macAddress string) (uint64, error) {
 
 func (b *BaseHandler) getKiosk(macAddress string) (models.KioskView, error) {
 	var kiosk models.KioskView
-	statement := "SELECT id, name ,macadress_wifi, macadress_ethernet , location , name_lotery , name_casino , date_start  , date_end  , status , publicity, home_page, scan_page , result_page            ,general_rules,specific_rules, secret  , secret_length , updated_at , updated_at_parameters FROM kiosk_view WHERE macadress_wifi = ? OR macadress_ethernet = ?"
+	statement := "SELECT id, name ,macadress_wifi, macadress_ethernet , location , name_lotery , name_casino , date_start  , date_end  , status , client_data , publicity, home_page, scan_page , result_page ,general_rules,specific_rules, secret  , secret_length , updated_at , updated_at_parameters FROM kiosk_view WHERE macadress_wifi = ? OR macadress_ethernet = ?"
 
 	db := b.db.GetDB()
-	err := db.QueryRow(statement, macAddress, macAddress).Scan(&kiosk.ID, &kiosk.Name, &kiosk.MacadressWifi, &kiosk.MacadressEthernet, &kiosk.Location, &kiosk.NameLotery, &kiosk.NameCasino, &kiosk.DateStart, &kiosk.DateEnd, &kiosk.Status, &kiosk.Publicity, &kiosk.HomePage, &kiosk.ScanPage, &kiosk.ResultPage, &kiosk.GeneralRules, &kiosk.SpecificRules, &kiosk.Secret, &kiosk.SecretLength, &kiosk.UpdatedAt, &kiosk.UpdatedAtParameters)
+	err := db.QueryRow(statement, macAddress, macAddress).Scan(&kiosk.ID, &kiosk.Name, &kiosk.MacadressWifi, &kiosk.MacadressEthernet, &kiosk.Location, &kiosk.NameLotery, &kiosk.NameCasino, &kiosk.DateStart, &kiosk.DateEnd, &kiosk.Status, &kiosk.ClientData, &kiosk.Publicity, &kiosk.HomePage, &kiosk.ScanPage, &kiosk.ResultPage, &kiosk.GeneralRules, &kiosk.SpecificRules, &kiosk.Secret, &kiosk.SecretLength, &kiosk.UpdatedAt, &kiosk.UpdatedAtParameters)
 	if err != nil {
 		return kiosk, err
 	}

5af4ddf4524640b751bd3ce8c372ecf1ff107af2
Johann six <six.johann@gmail.com>
2024-08-29 16:18:50 +0200

    add new field and refacto field

--- internal/models/tickets.go
+++ internal/models/tickets.go
@@ -1,28 +1,39 @@
 package models
 
-import "time"
+import (
+	"lotery_viking/internal/utils"
+	"time"
+)
 
 type Tickets struct {
 	BaseModel
 	KioskID      uint64     `db:"kiosk_id" json:"kiosk_id"`
 	Kiosk        *Kiosks    `json:"kiosk,omitempty"`
-	IDReward     uint64     `db:"id_reward" json:"id_reward,omitempty"`
-	Reward       *Rewards   `json:"reward,omitempty"`
+	IDReward     *uint64    `db:"id_reward" json:"id_reward"`
 	TicketNumber string     `db:"ticket_number" json:"ticket_number"`
+	ClientPhone  *string    `db:"client_phone" json:"client_phone"`
 	Claim        bool       `db:"claim" json:"claim"`
-	EntryScan    time.Time  `db:"entry_scan" json:"entry_scan"`
-	ExitScan     *time.Time `db:"exit_scan" json:"exit_scan,omitempty"`
+	EntryScan    *time.Time `db:"entry_scan" json:"entry_scan"`
+	ExitScan     *time.Time `db:"exit_scan" json:"exit_scan"`
+	RewardName   *string    `json:"reward_name"`
+	RewardBigWin *bool      `json:"big_win"`
+	RewardImage  *string    `json:"reward_image"`
 }
 
-func (t Tickets) SetEntryScanNow() {
-	t.EntryScan = time.Now()
+func (t *Tickets) SetEntryScanNow() {
+	now := time.Now()
+	t.EntryScan = &now
 }
 
-func (t Tickets) SetExitScanNow() {
+func (t *Tickets) SetExitScanNow() {
 	now := time.Now()
 	t.ExitScan = &now
 }
 
-func (t Tickets) isClaim() {
+func (t *Tickets) isClaimed() bool {
+	return t.Claim
+}
 
+func (t *Tickets) IsValid(secret string, secretLength int) bool {
+	return utils.DecryptCode(secret, secretLength, t.TicketNumber)
 }

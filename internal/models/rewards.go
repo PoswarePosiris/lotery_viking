8158f177a66167118719ccfece16cc21ebf01369
Johann six <six.johann@gmail.com>
2024-08-29 16:18:36 +0200

    add field and fix issue on field null

--- internal/models/rewards.go
+++ internal/models/rewards.go
@@ -3,6 +3,7 @@ package models
 type Rewards struct {
 	BaseModel
 	Name     string  `db:"name" json:"name"`
-	IdImages uint64  `db:"id_images" json:"image,omitempty"`
-	Images   *Images `json:"images,omitempty"`
+	BigWin   bool    `db:"big_win" json:"big_win"`
+	IdImages uint64  `db:"id_images" json:"id_images,omitempty"`
+	Image    *Images `json:"image,omitempty"`
 }

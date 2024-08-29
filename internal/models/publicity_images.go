669e6f8ccc3bb0ab290bbeb22d99a8978076137e
Johann six <six.johann@gmail.com>
2024-08-29 16:18:15 +0200

    fix issu on non json

--- internal/models/publicity_images.go
+++ internal/models/publicity_images.go
@@ -2,8 +2,8 @@ package models
 
 type PublicityImages struct {
 	BaseModel
-	ParameterId uint64      `db:"parameter_id" json:"omitempty"`
-	Parameter   *Parameters `json:"parameter"`
-	ImageId     uint64      `db:"image_id" json:"omitempty"`
-	Image       *Images     ` json:"image"`
+	ParameterId uint64      `db:"parameter_id" json:"parameter_id,omitempty"`
+	Parameter   *Parameters `json:"parameter,omitempty"`
+	ImageId     uint64      `db:"image_id" json:"image_id,omitempty"`
+	Image       *Images     `json:"image,omitempty"`
 }

ca0c6adc603a6300dbab5cdbcac1e9a7eae618ab
Johann six <six.johann@gmail.com>
2024-08-29 10:09:39 +0200

    add cmd migrate seed drop serve for logic app

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -2,12 +2,48 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"lotery_viking/internal/database"
 	"lotery_viking/internal/server"
 	"os"
 )
 
 func main() {
 
+	if len(os.Args) < 2 {
+		fmt.Println("Usage: go run main.go [migrate|drop|seed|seed-clear|serve]")
+		os.Exit(1)
+	}
+
+	switch os.Args[1] {
+	case "migrate":
+		err := database.Migrate()
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println("Database migration completed successfully")
+	case "drop":
+		err := database.Drop()
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println("Database dropped successfully")
+	case "seed":
+		database.Seed(false)
+		fmt.Println("Database seeding completed successfully")
+	case "seed-clear":
+		database.Seed(true)
+		fmt.Println("Database seeding completed successfully")
+	case "serve":
+		startServer()
+	default:
+		fmt.Println("Usage: go run main.go [migrate|drop|seed|seed-clear|serve]")
+		os.Exit(1)
+	}
+
+}
+
+func startServer() {
 	server := server.NewServer()
 
 	// Get the host and port

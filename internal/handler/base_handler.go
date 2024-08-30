8064e67eca180da52849fc30208b715014e34e78
Johann six <six.johann@gmail.com>
2024-08-30 11:55:06 +0200

    update with null pointer

--- internal/handler/base_handler.go
+++ internal/handler/base_handler.go
@@ -1,6 +1,7 @@
 package handler
 
 import (
+	"database/sql"
 	"lotery_viking/internal/database"
 	"lotery_viking/internal/models"
 )
@@ -21,14 +22,43 @@ func (b *BaseHandler) getKioskId(macAddress string) (uint64, error) {
 	return id, nil
 }
 
-func (b *BaseHandler) getKiosk(macAddress string) (models.KioskView, error) {
-	var kiosk models.KioskView
+func (b *BaseHandler) getKiosk(macAddress string) (*models.KioskView, error) {
+	// var kiosk models.KioskView
+	kiosk := &models.KioskView{}
 	statement := "SELECT id, name ,macadress_wifi, macadress_ethernet , location , name_lotery , name_casino , date_start  , date_end  , status , client_data , publicity, home_page, scan_page , result_page ,general_rules,specific_rules, secret  , secret_length , updated_at , updated_at_parameters FROM kiosk_view WHERE macadress_wifi = ? OR macadress_ethernet = ?"
 
 	db := b.db.GetDB()
-	err := db.QueryRow(statement, macAddress, macAddress).Scan(&kiosk.ID, &kiosk.Name, &kiosk.MacadressWifi, &kiosk.MacadressEthernet, &kiosk.Location, &kiosk.NameLotery, &kiosk.NameCasino, &kiosk.DateStart, &kiosk.DateEnd, &kiosk.Status, &kiosk.ClientData, &kiosk.Publicity, &kiosk.HomePage, &kiosk.ScanPage, &kiosk.ResultPage, &kiosk.GeneralRules, &kiosk.SpecificRules, &kiosk.Secret, &kiosk.SecretLength, &kiosk.UpdatedAt, &kiosk.UpdatedAtParameters)
+	var publicityNull, homePageNull, scanPageNull, resultPageNull sql.NullString
+	var specificRulesNull sql.NullString
+	err := db.QueryRow(statement, macAddress, macAddress).Scan(&kiosk.ID, &kiosk.Name, &kiosk.MacadressWifi, &kiosk.MacadressEthernet, &kiosk.Location, &kiosk.NameLotery, &kiosk.NameCasino, &kiosk.DateStart, &kiosk.DateEnd, &kiosk.Status, &kiosk.ClientData, &publicityNull, &homePageNull, &scanPageNull, &resultPageNull, &kiosk.GeneralRules, &specificRulesNull, &kiosk.Secret, &kiosk.SecretLength, &kiosk.UpdatedAt, &kiosk.UpdatedAtParameters)
 	if err != nil {
-		return kiosk, err
+		return nil, err
 	}
+	if publicityNull.Valid {
+		kiosk.Publicity = &publicityNull.String
+	} else {
+		kiosk.Publicity = nil
+	}
+	if homePageNull.Valid {
+		kiosk.HomePage = &homePageNull.String
+	} else {
+		kiosk.HomePage = nil
+	}
+	if scanPageNull.Valid {
+		kiosk.ScanPage = &scanPageNull.String
+	} else {
+		kiosk.ScanPage = nil
+	}
+	if resultPageNull.Valid {
+		kiosk.ResultPage = &resultPageNull.String
+	} else {
+		kiosk.ResultPage = nil
+	}
+	if specificRulesNull.Valid {
+		kiosk.SpecificRules = &specificRulesNull.String
+	} else {
+		kiosk.SpecificRules = nil
+	}
+
 	return kiosk, nil
 }

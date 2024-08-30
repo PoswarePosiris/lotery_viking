9d264cbf61576813f1698a5b3f5970940ab66c9b
Johann six <six.johann@gmail.com>
2024-08-30 12:54:18 +0200

    remove comment

--- internal/database/migrate.go
+++ internal/database/migrate.go
@@ -15,8 +15,6 @@ var schema embed.FS
 func Migrate() error {
 	db, err := sql.Open("mysql", fmt.Sprintf("%s:%s@tcp(%s:%s)/", username, password, host, port))
 	if err != nil {
-		// This will not be a connection error, but a DSN parse error or
-		// another initialization error.
 		log.Fatal(err)
 	}
 	defer db.Close()
@@ -36,7 +34,6 @@ func Migrate() error {
 		return err
 	}
 
-	// Convert the schema to a string and print it for debugging
 	requests := string(sqlFile)
 
 	// Split the SQL file into individual statements
@@ -61,8 +58,6 @@ func Migrate() error {
 func Drop() error {
 	db, err := sql.Open("mysql", fmt.Sprintf("%s:%s@tcp(%s:%s)/", username, password, host, port))
 	if err != nil {
-		// This will not be a connection error, but a DSN parse error or
-		// another initialization error.
 		log.Fatal(err)
 	}
 	defer db.Close()

ebf1779a4696fdc9a4696b636c41394a691a7007
Johann six <six.johann@gmail.com>
2024-08-30 11:54:02 +0200

    add Data for convert the image. later

--- internal/models/images.go
+++ internal/models/images.go
@@ -5,6 +5,7 @@ type Images struct {
 	Url    string `db:"url" json:"url"`
 	Name   string `db:"name" json:"name"`
 	Format string `db:"format" json:"format"`
+	Data   []byte `json:"-"`
 }
 
 // GetUrl returns the model url. full path
@@ -20,4 +21,19 @@ func (m *Images) GetName() string {
 // GetFormat returns the model format.
 func (m *Images) GetFormat() string {
 	return m.Format
+}
+
+// GetData returns the model data.
+func (m *Images) GetData() []byte {
+	return m.Data
+}
+
+// SetData set the model data.
+func (m *Images) SetData(data []byte) {
+	m.Data = data
+}
+
+// Get size in kbytes of the image
+func (m *Images) GetSize() int {
+	return len(m.Data) / 1024
 }

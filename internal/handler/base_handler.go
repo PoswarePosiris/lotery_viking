48e4871220b3887e2c7cb7a07697080b19d8cc5f
Johann six <six.johann@gmail.com>
2024-09-11 12:25:30 +0200

    update with column name change and add new func for images

--- internal/handler/base_handler.go
+++ internal/handler/base_handler.go
@@ -2,8 +2,11 @@ package handler
 
 import (
 	"database/sql"
+	"fmt"
 	"lotery_viking/internal/database"
 	"lotery_viking/internal/models"
+	"os"
+	"strings"
 )
 
 type BaseHandler struct {
@@ -25,40 +28,103 @@ func (b *BaseHandler) getKioskId(macAddress string) (uint64, error) {
 func (b *BaseHandler) getKiosk(macAddress string) (*models.KioskView, error) {
 	// var kiosk models.KioskView
 	kiosk := &models.KioskView{}
-	statement := "SELECT id, name ,macadress_wifi, macadress_ethernet , location , name_lotery , name_casino , date_start  , date_end  , status , client_data , publicity, home_page, scan_page , result_page ,general_rules,specific_rules, secret  , secret_length , updated_at , updated_at_parameters FROM kiosk_view WHERE macadress_wifi = ? OR macadress_ethernet = ?"
+	statement := "SELECT id, name ,macadress_wifi, macadress_ethernet , location , name_lotery , name_casino , date_start  , date_end  , status , client_data , home_page, client_page , result_page ,general_rules,specific_rules, secret  , secret_length , updated_at , updated_at_parameters FROM kiosk_view WHERE macadress_wifi = ? OR macadress_ethernet = ?"
 
 	db := b.db.GetDB()
-	var publicityNull, homePageNull, scanPageNull, resultPageNull sql.NullString
+	var homePageIdNull, clientPageIdNull, resultPageIdNull sql.NullInt64
 	var specificRulesNull sql.NullString
-	err := db.QueryRow(statement, macAddress, macAddress).Scan(&kiosk.ID, &kiosk.Name, &kiosk.MacadressWifi, &kiosk.MacadressEthernet, &kiosk.Location, &kiosk.NameLotery, &kiosk.NameCasino, &kiosk.DateStart, &kiosk.DateEnd, &kiosk.Status, &kiosk.ClientData, &publicityNull, &homePageNull, &scanPageNull, &resultPageNull, &kiosk.GeneralRules, &specificRulesNull, &kiosk.Secret, &kiosk.SecretLength, &kiosk.UpdatedAt, &kiosk.UpdatedAtParameters)
+	err := db.QueryRow(statement, macAddress, macAddress).Scan(&kiosk.ID, &kiosk.Name, &kiosk.MacadressWifi, &kiosk.MacadressEthernet, &kiosk.Location, &kiosk.NameLotery, &kiosk.NameCasino, &kiosk.DateStart, &kiosk.DateEnd, &kiosk.Status, &kiosk.ClientData, &homePageIdNull, &clientPageIdNull, &resultPageIdNull, &kiosk.GeneralRules, &specificRulesNull, &kiosk.Secret, &kiosk.SecretLength, &kiosk.UpdatedAt, &kiosk.UpdatedAtParameters)
 	if err != nil {
 		return nil, err
 	}
-	if publicityNull.Valid {
-		kiosk.Publicity = &publicityNull.String
-	} else {
-		kiosk.Publicity = nil
+	if homePageIdNull.Valid {
+		homePageId := uint64(homePageIdNull.Int64)
+		kiosk.HomePageId = &homePageId
 	}
-	if homePageNull.Valid {
-		kiosk.HomePage = &homePageNull.String
-	} else {
-		kiosk.HomePage = nil
+	if clientPageIdNull.Valid {
+		clientPageId := uint64(clientPageIdNull.Int64)
+		kiosk.ClientPageId = &clientPageId
 	}
-	if scanPageNull.Valid {
-		kiosk.ScanPage = &scanPageNull.String
-	} else {
-		kiosk.ScanPage = nil
-	}
-	if resultPageNull.Valid {
-		kiosk.ResultPage = &resultPageNull.String
-	} else {
-		kiosk.ResultPage = nil
+	if resultPageIdNull.Valid {
+		resultPageId := uint64(resultPageIdNull.Int64)
+		kiosk.ResultPageId = &resultPageId
 	}
 	if specificRulesNull.Valid {
 		kiosk.SpecificRules = &specificRulesNull.String
-	} else {
-		kiosk.SpecificRules = nil
 	}
 
 	return kiosk, nil
+}
+
+func (b *BaseHandler) GetPathImage(name string, format string) string {
+	haveSSL := os.Getenv("HTTP")
+	basePath := os.Getenv("HOST")
+	port := os.Getenv("PORT")
+
+	if haveSSL == "" {
+		haveSSL = "http"
+	}
+
+	if basePath == "" {
+		basePath = "localhost"
+	}
+	if port == "" {
+		port = "8080"
+	}
+
+	return fmt.Sprintf("%s://%s:%s/kiosk_images/%s.%s", haveSSL, basePath, port, name, format)
+}
+
+func (b *BaseHandler) getImagesFromList(list []uint64) ([]models.Images, error) {
+	var images []models.Images
+
+	placeholders := make([]string, len(list))
+	for i := range list {
+		placeholders[i] = "?"
+	}
+
+	query := fmt.Sprintf("SELECT id, name, url FROM images WHERE id IN (%s)", strings.Join(placeholders, ", "))
+
+	// Prepare the query
+	db := b.db.GetDB()
+	rows, err := db.Query(query, convertUint64SliceToInterfaceSlice(list)...)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	// Iterate through the result set
+	for rows.Next() {
+		var image models.Images
+		var url sql.NullString
+		err = rows.Scan(&image.ID, &image.Name, &url, &image.Format)
+		if err != nil {
+			return nil, err
+		}
+		if url.Valid {
+			image.Url = &url.String
+		} else {
+			// Use getPathImage to construct the URL
+			fullPath := b.GetPathImage(image.Name, image.Format)
+			image.Url = &fullPath
+		}
+
+		images = append(images, image)
+	}
+
+	// Check for errors encountered during iteration
+	if err = rows.Err(); err != nil {
+		return nil, err
+	}
+
+	return images, nil
+}
+
+// Helper function to convert []uint64 to []interface{} for query
+func convertUint64SliceToInterfaceSlice(slice []uint64) []interface{} {
+	result := make([]interface{}, len(slice))
+	for i, v := range slice {
+		result[i] = v
+	}
+	return result
 }

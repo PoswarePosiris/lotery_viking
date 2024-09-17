4371187bcb532e168c38586952c71cf96c9e07ec
Johann six <six.johann@gmail.com>
2024-09-17 11:49:13 +0200

    update model and add new table and refacto foreing key

--- internal/models/parameters.go
+++ internal/models/parameters.go
@@ -9,20 +9,19 @@ const (
 
 type Parameters struct {
 	BaseModel
-	NameLotery    string  `db:"name_lotery" json:"name_lotery"`
-	NameCasino    string  `db:"name_casino" json:"name_casino"`
-	DateStart     string  `db:"date_start" json:"date_start"`
-	DateEnd       string  `db:"date_end" json:"date_end"`
-	Status        Status  `db:"status" json:"status"`
-	ClientData    bool    `db:"client_data" json:"client_data"`
-	HomePageId    uint64  `db:"home_page" json:"home_page_id"`
-	HomePage      *Images `json:"home_page,omitempty"`
-	ClientPageId  uint64  `db:"client_page" json:"client_page_id"`
-	ClientPage    *Images `json:"client_page,omitempty"`
-	ResultPageId  uint64  `db:"result_page" json:"result_page_id"`
-	ResultPage    *Images `json:"result_page,omitempty"`
-	GeneralRules  string  `db:"general_rules" json:"general_rules"`
-	SpecificRules string  `db:"specific_rules" json:"specific_rules"`
-	Secret        string  `db:"secret" json:"secret"`
-	SecretLength  int     `db:"secret_length" json:"secret_length"`
+	NameLotery   string  `db:"name_lotery" json:"name_lotery"`
+	NameCasino   string  `db:"name_casino" json:"name_casino"`
+	DateStart    string  `db:"date_start" json:"date_start"`
+	DateEnd      string  `db:"date_end" json:"date_end"`
+	Status       Status  `db:"status" json:"status"`
+	ClientData   bool    `db:"client_data" json:"client_data"`
+	HomePageId   uint64  `db:"home_page" json:"home_page_id"`
+	HomePage     *Images `json:"home_page,omitempty"`
+	ClientPageId uint64  `db:"client_page" json:"client_page_id"`
+	ClientPage   *Images `json:"client_page,omitempty"`
+	ResultPageId uint64  `db:"result_page" json:"result_page_id"`
+	ResultPage   *Images `json:"result_page,omitempty"`
+	GeneralRules string  `db:"general_rules" json:"general_rules"`
+	Secret       string  `db:"secret" json:"secret"`
+	SecretLength int     `db:"secret_length" json:"secret_length"`
 }
--- internal/models/publicity_images.go
+++ internal/models/publicity_images.go
@@ -1,9 +1,11 @@
 package models
 
 type PublicityImages struct {
-	BaseModel
+	ID          uint64      `db:"id" json:"id,omitempty"`
 	ParameterId uint64      `db:"parameter_id" json:"parameter_id,omitempty"`
 	Parameter   *Parameters `json:"parameter,omitempty"`
+	KioskId     uint64      `db:"kiosk_id" json:"kiosk_id,omitempty"`
+	Kiosk       *Kiosks     `json:"kiosk,omitempty"`
 	ImageId     uint64      `db:"image_id" json:"image_id,omitempty"`
 	Image       *Images     `json:"image,omitempty"`
 }
